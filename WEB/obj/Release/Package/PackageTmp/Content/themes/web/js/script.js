3d0adb7f0a8872fc671dc52b6814960de5b0913d
trangqt2908 <66157201+trangqt2908@users.noreply.github.com>
2023-04-17 22:14:25 +0700

    update

--- WEB/obj/Release/Package/PackageTmp/Content/themes/web/js/script.js
+++ WEB/obj/Release/Package/PackageTmp/Content/themes/web/js/script.js
@@ -458,10 +458,10 @@
                 items: 2
             },
             992: {
-                items: 2
+                items: 3
             },
             1200: {
-                items: 2
+                items: 3
             }
         }
     });
